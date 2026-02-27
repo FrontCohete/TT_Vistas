4fc2f4dcc2d3bbd3c1916ae3687ecb0240771e7b
Front_Cohete <jcoheter.fe@gmail.com>
2026-02-26 22:22:10 -0600

    nav
    Falta correción de items

--- Vistas/POR HACER/vistas-pruebas/src/App.jsx
+++ Vistas/POR HACER/vistas-pruebas/src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import './App.css';
-import NavAdmin from './components/NavAdmin';
+import NavAdmin from './components/NavAdmin.jsx';
 
 function App () {
   return(
