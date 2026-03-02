9d3dc0e7db5eda90a88b10c0c16d8aa84b855225
Front_Cohete <jcoheter.fe@gmail.com>
2026-03-02 15:53:14 -0600

    Formulario inicio
    Comienzo formularios

--- Vistas/POR HACER/vistas-pruebas/src/App.jsx
+++ Vistas/POR HACER/vistas-pruebas/src/App.jsx
@@ -1,11 +1,13 @@
 import React, { useState } from 'react';
 import './App.css';
 import NavAdmin from './components/NavAdmin.jsx';
+import Form_PreR from './components/FormPreReg.jsx';
 
 function App () {
   return(
     <>
       <NavAdmin/>
+      <Form_PreR/>
     </>
 
   )
---
+++ Vistas/POR HACER/vistas-pruebas/src/components/FormPreReg.jsx
@@ -0,0 +1,16 @@
+import { useState, useRef } from "react";
+import { Link } from "react-router-dom"; // Mantenemos este para el dropdown
+import 'normalize.css';
+import '../assets/css/form_prereg.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+export default function Form_PreR(){
+    return(
+        <>
+            <Main>
+                <h1>Preregistro</h1><span> para empresas</span>
+            </Main>
+        </>
+    );
+}
